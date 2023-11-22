cad4728d451c6b50f6c80a37ace9e0fcaa8c6fa8
Francesco Maccantelli <50288792+macca0612@users.noreply.github.com>
2023-11-22 15:34:00 +0100

    publish repo

---
+++ process_dataset.py
@@ -0,0 +1,46 @@
+import os
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set the path to the GTZAN dataset
+general_path = 'data/gtzan'
+
+# Function to create and save the full MEL spectrogram
+def process_and_save_full_mel(audio_path, output_folder, genre, filename, hop_length=512):
+    # Load audio file
+    y, sr = librosa.load(audio_path)
+
+    # Trim silent portions
+    y, _ = librosa.effects.trim(y)
+
+    # Calculate MEL spectrogram
+    S = librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length)
+    S_DB = librosa.amplitude_to_db(S, ref=np.max)
+    
+    # Create the genre folder if it doesn't exist
+    genre_folder = os.path.join(output_folder, genre)
+    if not os.path.exists(genre_folder):
+        os.makedirs(genre_folder)
+
+    # Plot and save the full MEL spectrogram without title or color bar
+    plt.figure(figsize=(16, 6))
+    librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', cmap='cool')
+
+    # Save the figure without title or color bar
+    output_filename = os.path.join(output_folder, f"{genre}/{genre}.{filename[:-4]}.png")
+    plt.axis('off')
+    plt.savefig(output_filename, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+# Process each audio file in the dataset
+genres = os.listdir(f'{general_path}/genres_original')
+output_folder = 'data/gtzan/images_MEL'
+
+for genre in genres:
+    genre_path = os.path.join(f'{general_path}/genres_original', genre)
+
+    for filename in os.listdir(genre_path):
+        audio_path = os.path.join(genre_path, filename)
+        process_and_save_full_mel(audio_path, output_folder, genre, filename)
